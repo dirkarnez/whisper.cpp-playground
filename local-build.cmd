deee6394eacad4247a333b5378a543ca746eb0c7
dirkarnez <smalldirkalex@gmail.com>
2024-08-07 18:50:14 +0800

    - wip

--- local-build.cmd
+++ local-build.cmd
@@ -4,21 +4,25 @@ cd /d %~dp0
 set DOWNLOADS_DIR=%USERPROFILE%\Downloads
 set DOWNLOADS_DIR_LINUX=%DOWNLOADS_DIR:\=/%
 
-SET PATH=^
-%DOWNLOADS_DIR%\PortableGit\bin;^
-%DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64;^
-%DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin;^
-%DOWNLOADS_DIR%\cmake-3.26.1-windows-x86_64\bin;
+@REM SET PATH=^
+@REM %DOWNLOADS_DIR%\PortableGit\bin;^
+@REM %DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64;^
+@REM %DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin;^
+@REM %DOWNLOADS_DIR%\cmake-3.26.1-windows-x86_64\bin;
 
 @REM set PATH=^
 @REM D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64;^
 @REM D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin;^
 @REM D:\Softwares\cmake-3.23.0-rc1-windows-x86_64\bin;
 
+echo %PATH%
 cmake.exe -G"MinGW Makefiles" ^
 -DCMAKE_BUILD_TYPE=Debug ^
+-D
+D:\cpp_libraries\whisper.cpp-v1.5.4-winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1
 -B./cmake-build &&^
 cd cmake-build &&^
 cmake --build . &&^
-echo "Successful build"
+echo "Successful build" &&^
+cd ..
 pause
